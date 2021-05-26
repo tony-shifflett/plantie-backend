b7354a913ee68de228fed770d0e047d57debded4
youngjerald239 <youngjerald239@gmail.com>
2021-05-26 14:57:46 -0500

    updated info

--- controllers/Plant.js
+++ controllers/Plant.js
@@ -8,18 +8,17 @@ const DailyTask = require("./DailyTask")
 // SEED DATA FOR Plant ROUTE
 const plantSeed = [
     { 
-        
-        binomial: "Juniperus chinensis 'Shimpaku' ",
-        nickname:"",
-        type:"Bonsai Tree",
-       img: "https://images.unsplash.com/photo-1579450841234-49351e3a312b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9uc2FpJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
-       frequency: "2/week",
-       tempature:"20-24˚F",
-       water: "150ml",
-       sunlight:"Indirect light",
-       info: "They say that bonsai isn't just a plant, it's a way of life. Bonsai trees require regular care and maintenance. ... For beginners, Juniper bonsai trees are the easiest to care for so they're perfect for novice bonsai enthusiasts.", 
-       isVisible: true,
-       task:{DailyTask,water},
+      binomial: "Juniperus chinensis 'Shimpaku' ",
+      nickname:"",
+      type:"Bonsai Tree",
+      img: "https://images.unsplash.com/photo-1579450841234-49351e3a312b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9uc2FpJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      frequency: "2/week",
+      tempature:"20-24˚F",
+      water: "150ml",
+      sunlight:"Indirect light",
+      info: "They say that bonsai isn't just a plant, it's a way of life. Bonsai trees require regular care and maintenance. ... For beginners, Juniper bonsai trees are the easiest to care for so they're perfect for novice bonsai enthusiasts.", 
+      isVisible: true,
+      tasks:[""],
       },
     {
         
@@ -33,7 +32,7 @@ const plantSeed = [
         sunlight:"Direct light",
         info: "Gypsophila is a genus of flowering plants in the carnation family, Caryophyllaceae. They are native to Eurasia, Africa, Australia, and the Pacific Islands.",
         isVisible:true,
-        task:{DailyTask,water}     
+        tasks:[""]     
      },
      {  
         
@@ -47,7 +46,245 @@ const plantSeed = [
         sunlight:"Direct light",
         info: "Small and prickly. Harmful to touch!",
         isVisible:true,
-        task:{DailyTask,repotting}    
+        tasks:[""]    
+     },
+
+     { 
+      binomial: "Trichosanthes cucumerina",
+      nickname:"",
+      type:"Snake Gourd Flower",
+      img: "https://upload.wikimedia.org/wikipedia/commons/0/0d/A_Beautiful_flower_of_snake_gourd_plant_%28Trichosanthes_cucumerina%29.jpg",
+      frequency: "4/week",
+      tempature:"20-24˚F",
+      water: "180ml",
+      sunlight:"Direct light",
+      info: "The snake gourd is an annual plant with forked tendrils and kidney- or heart-shaped leaves that are sometimes palmately lobed.", 
+      isVisible: true,
+      tasks:[""],
+      },
+    {
+        
+        binomial:"Magnolia fraseri",
+        nickname:"",
+        type:"Magnolia Flower",
+        img: "https://images.unsplash.com/photo-1474112704314-8162b7749a90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        frequency: "1/3-4 days",
+        temperature:"34 - 36˚F",
+        water: "120ml",
+        sunlight:"Direct light",
+        info: "Magnolias are spreading, evergreen or deciduous trees or shrubs, characterised by large fragrant flowers which may be bowl-shaped or star-shaped, in shades of white, pink, purple, green or yellow.",
+        isVisible:true,
+        tasks:[""]     
+     },
+     {  
+        
+        binomial:"Schlumbergera",
+        nickname:"",
+        type:"Cactus",
+        img: "https://images.unsplash.com/photo-1508022057371-4f937727f440?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdXNlJTIwY2FjdHVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        frequency: "none",
+        temperature:"34 - 36˚F",
+        water: "none",
+        sunlight:"Direct light",
+        info: "Schlumbergera is a small genus of cacti with six to nine species found in the coastal mountains of south-eastern Brazil.",
+        isVisible:true,
+        tasks:[""]    
+     },
+
+     { 
+      binomial: "Juniperus chinensis 'Shimpaku' ",
+      nickname:"",
+      type:"Bonsai Tree",
+      img: "https://images.unsplash.com/photo-1579450841234-49351e3a312b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9uc2FpJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      frequency: "2/week",
+      tempature:"20-24˚F",
+      water: "150ml",
+      sunlight:"Indirect light",
+      info: "They say that bonsai isn't just a plant, it's a way of life. Bonsai trees require regular care and maintenance. ... For beginners, Juniper bonsai trees are the easiest to care for so they're perfect for novice bonsai enthusiasts.", 
+      isVisible: true,
+      tasks:[""],
+      },
+    {
+        
+        binomial:"genus Gypsophila",
+        nickname:"",
+        type:"Gypsophilia",
+        img: "https://images.unsplash.com/photo-1614876753060-0751185c9289?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80",
+        frequency: "1/3-4 days",
+        temperature:"34 - 36˚F",
+        water: "120ml",
+        sunlight:"Direct light",
+        info: "Gypsophila is a genus of flowering plants in the carnation family, Caryophyllaceae. They are native to Eurasia, Africa, Australia, and the Pacific Islands.",
+        isVisible:true,
+        tasks:[""]     
+     },
+     {  
+        
+        binomial:"Cactaceae",
+        nickname:"",
+        type:"Cactus",
+        img: "https://images.unsplash.com/photo-1508022057371-4f937727f440?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdXNlJTIwY2FjdHVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        frequency: "none",
+        temperature:"34 - 36˚F",
+        water: "none",
+        sunlight:"Direct light",
+        info: "Small and prickly. Harmful to touch!",
+        isVisible:true,
+        tasks:[""]    
+     },
+
+     { 
+      binomial: "Juniperus chinensis 'Shimpaku' ",
+      nickname:"",
+      type:"Bonsai Tree",
+      img: "https://images.unsplash.com/photo-1579450841234-49351e3a312b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9uc2FpJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      frequency: "2/week",
+      tempature:"20-24˚F",
+      water: "150ml",
+      sunlight:"Indirect light",
+      info: "They say that bonsai isn't just a plant, it's a way of life. Bonsai trees require regular care and maintenance. ... For beginners, Juniper bonsai trees are the easiest to care for so they're perfect for novice bonsai enthusiasts.", 
+      isVisible: true,
+      tasks:[""],
+      },
+    {
+        
+        binomial:"genus Gypsophila",
+        nickname:"",
+        type:"Gypsophilia",
+        img: "https://images.unsplash.com/photo-1614876753060-0751185c9289?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80",
+        frequency: "1/3-4 days",
+        temperature:"34 - 36˚F",
+        water: "120ml",
+        sunlight:"Direct light",
+        info: "Gypsophila is a genus of flowering plants in the carnation family, Caryophyllaceae. They are native to Eurasia, Africa, Australia, and the Pacific Islands.",
+        isVisible:true,
+        tasks:[""]     
+     },
+     {  
+        
+        binomial:"Cactaceae",
+        nickname:"",
+        type:"Cactus",
+        img: "https://images.unsplash.com/photo-1508022057371-4f937727f440?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdXNlJTIwY2FjdHVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        frequency: "none",
+        temperature:"34 - 36˚F",
+        water: "none",
+        sunlight:"Direct light",
+        info: "Small and prickly. Harmful to touch!",
+        isVisible:true,
+        tasks:[""]    
+     },
+
+     { 
+      binomial: "Juniperus chinensis 'Shimpaku' ",
+      nickname:"",
+      type:"Bonsai Tree",
+      img: "https://images.unsplash.com/photo-1579450841234-49351e3a312b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9uc2FpJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      frequency: "2/week",
+      tempature:"20-24˚F",
+      water: "150ml",
+      sunlight:"Indirect light",
+      info: "They say that bonsai isn't just a plant, it's a way of life. Bonsai trees require regular care and maintenance. ... For beginners, Juniper bonsai trees are the easiest to care for so they're perfect for novice bonsai enthusiasts.", 
+      isVisible: true,
+      tasks:[""],
+      },
+    {
+        
+        binomial:"genus Gypsophila",
+        nickname:"",
+        type:"Gypsophilia",
+        img: "https://images.unsplash.com/photo-1614876753060-0751185c9289?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80",
+        frequency: "1/3-4 days",
+        temperature:"34 - 36˚F",
+        water: "120ml",
+        sunlight:"Direct light",
+        info: "Gypsophila is a genus of flowering plants in the carnation family, Caryophyllaceae. They are native to Eurasia, Africa, Australia, and the Pacific Islands.",
+        isVisible:true,
+        tasks:[""]     
+     },
+     {  
+        
+        binomial:"Cactaceae",
+        nickname:"",
+        type:"Cactus",
+        img: "https://images.unsplash.com/photo-1508022057371-4f937727f440?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdXNlJTIwY2FjdHVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        frequency: "none",
+        temperature:"34 - 36˚F",
+        water: "none",
+        sunlight:"Direct light",
+        info: "Small and prickly. Harmful to touch!",
+        isVisible:true,
+        tasks:[""]    
+     },
+
+     { 
+      binomial: "Juniperus chinensis 'Shimpaku' ",
+      nickname:"",
+      type:"Bonsai Tree",
+      img: "https://images.unsplash.com/photo-1579450841234-49351e3a312b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9uc2FpJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      frequency: "2/week",
+      tempature:"20-24˚F",
+      water: "150ml",
+      sunlight:"Indirect light",
+      info: "They say that bonsai isn't just a plant, it's a way of life. Bonsai trees require regular care and maintenance. ... For beginners, Juniper bonsai trees are the easiest to care for so they're perfect for novice bonsai enthusiasts.", 
+      isVisible: true,
+      tasks:[""],
+      },
+    {
+        
+        binomial:"genus Gypsophila",
+        nickname:"",
+        type:"Gypsophilia",
+        img: "https://images.unsplash.com/photo-1614876753060-0751185c9289?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80",
+        frequency: "1/3-4 days",
+        temperature:"34 - 36˚F",
+        water: "120ml",
+        sunlight:"Direct light",
+        info: "Gypsophila is a genus of flowering plants in the carnation family, Caryophyllaceae. They are native to Eurasia, Africa, Australia, and the Pacific Islands.",
+        isVisible:true,
+        tasks:[""]     
+     },
+     {  
+        
+        binomial:"Cactaceae",
+        nickname:"",
+        type:"Cactus",
+        img: "https://images.unsplash.com/photo-1508022057371-4f937727f440?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdXNlJTIwY2FjdHVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        frequency: "none",
+        temperature:"34 - 36˚F",
+        water: "none",
+        sunlight:"Direct light",
+        info: "Small and prickly. Harmful to touch!",
+        isVisible:true,
+        tasks:[""]    
+     },
+
+     { 
+      binomial: "Juniperus chinensis 'Shimpaku' ",
+      nickname:"",
+      type:"Bonsai Tree",
+      img: "https://images.unsplash.com/photo-1579450841234-49351e3a312b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9uc2FpJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      frequency: "2/week",
+      tempature:"20-24˚F",
+      water: "150ml",
+      sunlight:"Indirect light",
+      info: "They say that bonsai isn't just a plant, it's a way of life. Bonsai trees require regular care and maintenance. ... For beginners, Juniper bonsai trees are the easiest to care for so they're perfect for novice bonsai enthusiasts.", 
+      isVisible: true,
+      tasks:[""],
+      },
+    {
+        
+        binomial:"genus Gypsophila",
+        nickname:"",
+        type:"Gypsophilia",
+        img: "https://images.unsplash.com/photo-1614876753060-0751185c9289?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80",
+        frequency: "1/3-4 days",
+        temperature:"34 - 36˚F",
+        water: "120ml",
+        sunlight:"Direct light",
+        info: "Gypsophila is a genus of flowering plants in the carnation family, Caryophyllaceae. They are native to Eurasia, Africa, Australia, and the Pacific Islands.",
+        isVisible:true,
+        tasks:[""]     
      },
    
   ];
@@ -64,6 +301,16 @@ router.get("/seed", async (req, res) => {
       await Plant.create(plantSeed);
       // get full list of plants to confirm seeding worked
       const plants = await Plant.find({});
+     for (i=0; i<plants.length; i++) {
+       const dailytasks = await DailyTask.find({});
+       const dailytask = await DailyTask.findById(req.params.id)
+    console.log("DailyTask")
+    const plants = await Plant.findByIdAndUpdate(req.params.plantId, {$push: {tasks: dailytask.id},
+    new: true
+    })
+ 
+  }
+      
       // return full list of plants as JSON
       res.json(plants);
     } catch (error) {
